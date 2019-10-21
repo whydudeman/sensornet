e0293681270a1374289790b57c349d07f287b828
Sayat Bekbolat <bekbolat.sayat@gmail.com>
2019-10-21 18:54:02 +0600

    Now device sends video frame and all frames will be proccessed in server device

--- pythonTest/client.py
+++ pythonTest/client.py
@@ -7,7 +7,7 @@ import pickle
 import zlib
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.0.170', 8585))
+client_socket.connect(('localhost', 8485))
 connection = client_socket.makefile('wb')
 
 cam = cv2.VideoCapture(0)
--- pythonTest/server.py
+++ pythonTest/server.py
@@ -5,6 +5,54 @@ import pickle
 import numpy as np
 import struct ## new
 import zlib
+import math
+from sklearn import neighbors
+import os
+import os.path
+from PIL import Image, ImageDraw
+import face_recognition
+from face_recognition.face_recognition_cli import image_files_in_folder
+def predict(get_frame, knn_clf=None, model_path="/dataset", distance_threshold=0.389):
+    """
+    Recognizes faces in given image using a trained KNN classifier
+    :param X_img_path: path to image to be recognized
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
+    """
+
+    # Load a trained KNN model (if one was passed in)
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    # Load image file and find face locations
+   
+    X_face_locations = face_recognition.face_locations(get_frame)
+
+    if len(X_face_locations) == 0:
+        return []
+    # Find encodings for faces in the test iamge
+    faces_encodings = face_recognition.face_encodings(get_frame, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=2)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+    
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+
+
+    # STEP 1: Train the KNN classifier and save it to disk
+    # # Once the model is trained and saved, you can skip this step next time.
+    # print("Training KNN classifier...")
+    # classifier = train("train_dir", model_save_path="dataset/trained_knn_model.clf", n_neighbors=1)
+    # print("Training complete!")
 
 HOST=''
 PORT=8485
@@ -36,8 +84,29 @@ while True:
         data += conn.recv(4096)
     frame_data = data[:msg_size]
     data = data[msg_size:]
-
     frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
     frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    predictions=predict(frame,knn_clf=None,model_path="dataset/trained_knn_model.clf",distance_threshold=0.389)
+    for name, (top, right, bottom, left) in predictions:
+        # See if the face is a match for the known face(s)
+        
+
+        
+
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1) 
     cv2.imshow('ImageWindow',frame)
     cv2.waitKey(1)
