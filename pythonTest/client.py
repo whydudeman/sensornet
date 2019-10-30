a6b6210bfa8e1f6906ac4edcaad59ae70a0ad609
Sayat Bekbolat <bekbolat.sayat@gmail.com>
2019-10-30 17:37:10 +0600

    Server sends name of person and Client gets this name

--- pythonTest/client.py
+++ pythonTest/client.py
@@ -6,29 +6,25 @@ import time
 import pickle
 import zlib
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 8485))
-connection = client_socket.makefile('wb')
 
-cam = cv2.VideoCapture(0)
-
-cam.set(3, 320);
-cam.set(4, 240);
-
-img_counter = 0
-
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-
-while True:
-    ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame, encode_param)
-#    data = zlib.compress(pickle.dumps(frame, 0))
-    data = pickle.dumps(frame, 0)
-    size = len(data)
-
-
-    print("{}: {}".format(img_counter, size))
-    client_socket.sendall(struct.pack(">L", size) + data)
-    img_counter += 1
-
-cam.release()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+	client_socket.connect(('localhost', 8485))
+	connection = client_socket.makefile('wb')
+	cam = cv2.VideoCapture(0)
+	cam.set(3, 320);
+	cam.set(4, 240);
+	encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+	while True:
+	    ret, frame = cam.read()
+	    result, frame = cv2.imencode('.jpg', frame, encode_param)
+	    send_data = pickle.dumps(frame, 0)
+	    size = len(send_data)
+	    client_socket.sendall(struct.pack(">L", size) + send_data)
+	    client_data = client_socket.recv(1024)
+	    client_data.decode()
+	    if(client_data!="unknown"):
+	    	isKnown=True
+	    	start=time.time()
+	    	
+	    print(client_data)
+	cam.release()
